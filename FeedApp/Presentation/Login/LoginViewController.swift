be55ae454889a998364338bc7e49b8ce78171830
Ilya Biltuev <ilyabiltuev@gmail.com>
2022-03-13 00:06:02 +0300

    handle keyboard

--- FeedApp/Presentation/Login/LoginViewController.swift
+++ FeedApp/Presentation/Login/LoginViewController.swift
@@ -18,8 +18,8 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        startObservingKeyboard()
+        addKeyboardHideTapRecognizer()
     }
     
     @IBAction func signInButtonDidTap(_ sender: UIButton) {
@@ -33,14 +33,62 @@ class LoginViewController: UIViewController {
         authService.signIn(
             username: username,
             password: password
-        ) { [weak self] error in
+        ) { [weak self] result in
             DispatchQueue.main.async {
-                print(error)
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let code):
+                    print(code)
+                }
             }
         }
+    }
+    
+    // MARK: - Keyboard
+    
+    private func addKeyboardHideTapRecognizer() {
+        let tapOnView = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapOnView.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapOnView)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    private func startObservingKeyboard() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleKeyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleKeyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func handleKeyboardWillShow(notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.size.height, right: 0)
+        scrollView.contentInset = contentInsets
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+    
+    @objc private func handleKeyboardWillHide(notification: Notification) {
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
     }
 }
 
 extension LoginViewController: UITextFieldDelegate {
-    
+    // TODO
 }
